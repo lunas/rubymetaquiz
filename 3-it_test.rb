f79cc1167af3da2e3caa7bd1efc9a600747e2479
lunas <lukasnick@gmail.com>
2017-10-31 20:19:25 +0100

    Vain trial with ItBlock class

--- 3-it_test.rb
+++ 3-it_test.rb
@@ -10,6 +10,44 @@ require 'minitest/pride'
 #
 # You code stars here:
 
+module ArrayIt
+
+  class ItBlock
+
+    attr_reader :it
+
+    def initialize(item)
+      @it = item
+    end
+
+    def yield_to(&block)
+      block.call
+    end
+  end
+
+
+  def filter(&block)
+    tmp = []
+    for item in self do
+      # Doesn't work:
+      # block.define_singleton_method(:it) do
+      #   item
+      # end
+
+
+      it_block = ItBlock.new(item)
+
+      tmp << item if it_block.yield_to(&block)
+    end
+
+    tmp
+  end
+
+end
+
+class Array
+  include ArrayIt
+end
 
 # Your code ends here.
 
@@ -17,7 +55,9 @@ class ItTest < Minitest::Test
   def test_it_block
     array = %w(foo Bar baz)
     the_character_b = 'b'
-    assert_equal %w(baz), array.filter { it.start_with? the_character_b }
+    assert_equal %w(baz), array.filter {
+      it.start_with? the_character_b
+    }
   end
 
   def test_it_outside_block
